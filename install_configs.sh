4fd432b4ec073568a511430c2ca1c673372efc3b
Misha Lew <mishalew1@gmail.com>
2021-12-06 10:09:23 -0500

    updated install and added uninstall

--- install_configs.sh
+++ install_configs.sh
@@ -23,35 +23,78 @@ LIGHT_RED="\033[0;31m"
 RESTORE="\033[0m"
 
 
-# Bash aliases copy
+# Copy .bashrc_misha to ~ 
+if [ -f ~/.bashrc_misha ]; then
+    echo -e "${RED}NOT overriding exisiting .bashrc_misha"
+else
+    cp -n .bashrc_misha ~ 
+    echo -e "${GREEN}SUCCESS, copied .bashrc_misha"
+fi
+
+
+# Copy .bash_aliases to ~
 if [ -f ~/.bash_aliases ]; then
-    echo -e "${GREEN}Bash_aliases found, NOT overriding!"
+    echo -e "${RED}NOT overriding exisiting .bash_aliases"
 else
-    echo -e "${GREEN}Bash_aliases not found, copying new ones!"
-	cp -nv .bash_aliases ~/.bash_aliases 
+    cp -n .bash_aliases ~ 
+    echo -e "${GREEN}SUCCESS, copied .bash_aliases"
 fi
 
 
-# TMUX Conf setup
+# Copy bash_prompt_command to ~ 
+if [ -f ~/.bash_prompt_command ]; then
+    echo -e "${RED}NOT overriding exisiting .bash_prompt_command"
+else
+    cp -n .bash_prompt_command  ~ 
+    echo -e "${GREEN}SUCCESS, copied .bash_prompt_command"
+fi
+
+
+# Copy .vimrc to ~
+if [ -f ~/.vimrc ]; then
+    echo -e "${RED}NOT overriding exisiting .vimrc"
+else
+    cp -n .vimrc  ~
+    echo -e "${GREEN}SUCCESS, copied .vimrc"
+fi
+
+
+# Copy .tmux to ~
 if [ -f ~/.tmux.conf ]; then
-    echo -e "tmux.conf found, NOT overriding!"
+    echo -e "${RED}NOT overriding exisiting .tmux.conf"
 else
-    echo -e "tmux.conf not found, copying new ones!"
-	cp -nv .tmux.conf ~/.tmux.conf 
+    cp -n .tmux.conf  ~
+    echo -e "${GREEN}SUCCESS, copied .tmux.conf"
 fi
 
 
-# Add source bash_aliases on .bashrc
+# Add source bash_aliases in ~/.bashrc
 if grep -q "if \[ -f ~/.bash_aliases ]" ~/.bashrc; then
-	echo -e "bash_alias reference already configured${RESTORE}"	
+	echo -e "${RED}.bash_alias reference already configured${RESTORE}"	
 else
-	echo -e "adding reference to bash_aliases file in bashrc${RESTORE}"
+	echo -e "${GREEN}adding reference to bash_aliases file in bashrc${RESTORE}"
     cat >> ~/.bashrc << EOF
 if [ -f ~/.bash_aliases ]; then
 	. ~/.bash_aliases
 fi
+
 EOF
 fi
+
+
+# Add source bashrc_misha in ~/.bashrc
+if grep -q "if \[ -f ~/.bashrc_misha ]" ~/.bashrc; then
+	echo -e "${RED}.bashrc_misha reference already configured"	
+else
+	echo -e "${GREEN}.bashrc_misha reference added to .bashrc"
+    cat >> ~/.bashrc << EOF
+if [ -f ~/.bashrc_misha ]; then
+	. ~/.bashrc_misha
+fi
+
+EOF
+fi
+
 
 # Reload current environment
 source ~/.bashrc
---
+++ uninstall_configs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script uninstalls the config files previously installed
+# It first creates backups of the config files
+
+files=(
+.bashrc_misha 
+.bash_prompt_command 
+.vimrc 
+.tmux.conf 
+.bash_aliases
+)
+
+for file in "${files[@]}"; do
+    cp -n ~/"$file" ~/"$file".bak
+    rm ~/"$file";
+done
